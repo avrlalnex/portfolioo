64f28a5bbbb286da789280846ebf4c65ac1d53a5
Avril Lalaine <almlinaza@addu.edu.ph>
2025-06-07 18:11:33 +0800

    minor fixes

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,37 +1,44 @@
+import samgyup from '../assets/samgyup.png';
+import catalogue from '../assets/cat-alogue.png';
+import appsheet from '../assets/appsheet.png';
+import utility from '../assets/utility.png';
+import task from '../assets/task.png';
+import symp from '../assets/symp.png';
+
 export default [
 	{
 		title: "Samgyupsalamat Management System",
-		imgUrl: "/assets/samgyup.png",
+		imgUrl: samgyup,
 		stack: ["PHP", "MySQL", "JS"],
 		link: "https://github.com/avrlalnex/sad2-restaurant-management-system",
 	},
 	{
 		title: "Cat-alogue",
-		imgUrl: "/assets/cat-alogue.png",
+		imgUrl: catalogue,
 		stack: ["React", "Python", "Tailwind", "Django", "Vite"],
 		link: "https://github.com/avrlalnex/cat-alogue",
 	},
 	{
 		title: "Conference Scheduler App",
-		imgUrl: "/assets/appsheet.png",
+		imgUrl: appsheet,
 		stack: ["Appsheet"],
 		link: "https://www.appsheet.com/start/d52c43af-e986-4d27-80e9-f04a2290cffc",
 	},
 	{
 		title: "Utility Suite",
-		imgUrl: "/assets/utility.png",
+		imgUrl: utility,
 		stack: ["HTML", "CSS", "JS"],
 		link: "https://github.com/avrlalnex/avrillinaza",
 	},
 	{
 		title: "Task Management App",
-		imgUrl: "/assets/task.png",
+		imgUrl: task,
 		stack: ["Next.js", "TailwindCSS", "Storybook", "Shadcn"],
 		link: "https://www.figma.com/proto/wNR1uENQ4Cta0hdUHfYYdC/TEST-101---Task-Management-App?node-id=78-154&starting-point-node-id=78%3A154&t=OkMhG2pg3oNBCmtw-1",
 	},
 	{
 		title: "Pneumonia Detection",
-		imgUrl: "/assets/symp.png",
+		imgUrl: symp,
 		stack: ["Google Colab", "Python", "TensorFlow", "Keras"],
 		link: "https://colab.research.google.com/drive/1K7WeZ_g_UcX48qSjfvmtrR2TuBprEJ_g?usp=sharing",
 	},
--- src/data/prototype.js
+++ src/data/prototype.js
@@ -1,27 +1,31 @@
+import asana from '../assets/asana.png';
+import catalogue from '../assets/cat-alogue.png';
+import sis from '../assets/sis.png';
+import samgyup from '../assets/samgyup.png';
+
 export default [
 	{
 		title: "Asanazen: Mindful Movement",
-		imgUrl: "/assets/asana.png",
+		imgUrl: asana,
 		stack: ["figma"],
 		link: "https://www.figma.com/design/tKJ8YnG6x1jFuYwT1tr0si/HCI?node-id=38%3A2&t=lzmPOcG2umZ05WsQ-1",
 	},
 	{
 		title: "Cat-alogue: Cat Adoption",
-		imgUrl: "/assets/cat-alogue.png",
+		imgUrl: catalogue,
 		stack: ["figma"],
 		link: "https://www.figma.com/design/sj6Zp7xLhBuByelZg80h5D/Cat-alogue?node-id=0%3A1&t=WOWgRagyqWj99dsD-1",
 	},
 	{
 		title: "SIS: Student Information System",
-		imgUrl: "/assets/sis.png",
+		imgUrl: sis,
 		stack: ["figma"],
 		link: "https://www.figma.com/design/pnMVf6XpGBFIoruKaS86N1/sis?node-id=8%3A3&t=cp3SlJduQp46NrGS-1",
 	},
-    {
+	{
 		title: "Samgyupsalamat Management System",
-		imgUrl: "/assets/samgyup.png",
+		imgUrl: samgyup,
 		stack: ["figma"],
 		link: "https://www.figma.com/design/p7y4J00sjuiFdnjKJ1kp5P/Samgyupsalamat?node-id=0%3A1&t=JFKqNN2hg3qL5PTP-1",
 	},
-	
 ];
